a1a21662ca7ea5f5e3ecd7410ce9fefc46472c34
Sébastien BOURREAU <sebastien.bourreau@cooperl.com>
2026-01-27 16:58:37 +0100

    fix: servers list

--- env/serversList.sh
+++ env/serversList.sh
@@ -2,13 +2,5 @@
 
 ### Servers list
 declare -A servers=(
-  [GIGC]=2012
-  [GIGC1]=2012
-  [GIGCTEST]=2012
-  [GIGC1TEST]=2012
-  [CRD]=2012
-  [CRDTEST]=2012
-  [GIGCBK]=2012
-  [GIGC1BK]=2012
-  [CRDBK]=2012
+  [SERVERNAME]=port
 )
